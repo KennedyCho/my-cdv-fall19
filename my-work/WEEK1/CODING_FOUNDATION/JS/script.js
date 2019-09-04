67a2f52635819062496e79619234710cd1cdeec3
Kennedy Cambra-Cho <kennedy.cambracho@gmail.com>
2019-09-04 23:06:29 +0800

    exercise answer

---
+++ my-work/WEEK1/CODING_FOUNDATION/JS/script.js
@@ -0,0 +1,41 @@
+// recreate box with js
+// (createElement, className, appendChild)
+
+// //create div tag
+// let box = document.createElement("div");
+// //name the div class
+// box.className = 'box_style';
+// //add div box to body of html
+// document.body.appendChild(box)
+
+
+// let button = document.createElement("button");
+//
+// button.className = 'button_style';
+//
+// document.body.appendChild(button)
+
+// on click display box
+document.getElementById('button').addEventListener('click', display_box)
+
+
+// // test button and eventListener
+// function display_confirm() {
+//   console.log(document.getElementById('num_box').value);
+// }
+
+// show box when called
+function display_box() {
+
+  for (var i = 0; i < document.getElementById('num_box').value; i++) {
+    //create div tag
+    let box = document.createElement("div");
+    //name the div class
+    box.className = 'box_style';
+    //add div box to body of html
+    document.body.appendChild(box)
+
+  }
+
+
+}
--- my-work/WEEK1/CODING_FOUNDATION/script.js
+++
@@ -1,9 +0,0 @@
-
-document.getElementById('my_button').addEventListener('click', function display_box(num_box) {
-  //document.getElementById('num_box')
-  //for(var i=0; i < num_box; i++){
-  console.log('true');
-
-  }
-
-}))
