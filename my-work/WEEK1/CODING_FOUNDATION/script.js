42ee4fb2d333b42b716ae44efb64ce0c528fac67
Kennedy Cambra-Cho <kennedy.cambracho@gmail.com>
2019-09-04 16:55:37 +0800

    changes

---
+++ my-work/WEEK1/CODING_FOUNDATION/script.js
@@ -0,0 +1,9 @@
+
+document.getElementById('my_button').addEventListener('click', function display_box(num_box) {
+  //document.getElementById('num_box')
+  //for(var i=0; i < num_box; i++){
+  console.log('true');
+
+  }
+
+}))
