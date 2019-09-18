5dfc6f4cdbdae31a4c3ae377c83bc3126410447a
Kennedy Cambra-Cho <kennedy.cambracho@gmail.com>
2019-09-18 13:49:55 +0800

    rename folder

---
+++ my-work/WEEK03/IN-CLASS/DATA_FUNCTIONS/JS/script.js
@@ -0,0 +1,14 @@
+// 1. find *real* data (array containing JS objects) in JSON format
+// 2. load data (make it *console.loggable* (what a word!) in our script)
+// 3. concept: data and datapoint
+// 4. make on circle for each datapoint (size and position doesn't matter)
+// 5. concept: functions that "return"
+// 6. use function to position circles randomly
+// 7. realize "who" calls this function / why is there no `()`?
+// 8. concept: "passing value into function"
+// 9. let's assume: "D3 passes value into the data function"
+// 10. if D3 passes a value, how can we receive it?
+// 11. use *real* information to impact the x position
+// 12. let's assume: "D3 passes another value!"
+// 13. how can we receive that value?
+// 14. in which ways is D3 making our live easy?
