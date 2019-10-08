9f2559506b622b2ff78572efb9bd0ea69b2826ee
Kennedy Cambra-Cho <kennedy.cambracho@gmail.com>
2019-10-08 19:38:18 +0800

    a

--- my-work/WEEK05/DATA_ZINE_PROJ/middlescript.js
+++ my-work/WEEK05/DATA_ZINE_PROJ/middlescript.js
@@ -9,8 +9,9 @@ let viz = d3.select("#container")
 ;
 
 
-function gotData(incomingData){
-  console.log(incomingData);
+function gotData(incomingData, i){
+  // console.log(incomingData);
+
 
 // link elements to data
 // creates empty place holders
@@ -19,15 +20,40 @@ function gotData(incomingData){
     // same as .attr("class", "name")
       .classed("datagroup", true)
   ;
-  // timestamp (hour value) is being exported wrong from google sheet - json file
-  let timestampToHour = d3.timeFormat("%I %p");
 
-  // test
-  let test = timestampToHour(incomingData[0].timestamp);
-  console.log(test);
-  console.log(typeof(test));
+  // filter data to only have entries with no company
+  function noCompFilter(d) {
+
+    if(d.withCompany == 'no'){
+      return true;
+    }else {
+      return false;
+    }
 
   }
+  let noCompData = incomingData.filter(noCompFilter);
+  // console.log(filteredData);
+
+  if (datagroups.withCompany == 'no'){
+    datagroups.append('circle')
+              .attr("cx", randomNumber)
+              .attr("cy", 200)
+              .attr("r", 20)
+              .attr("fill", getColor)
+  }
+
+
+
+  // // timestamp (hour value) is being exported wrong from google sheet - json file
+  // let timestampToHour = d3.timeFormat("%Y");
+  // // d3.timeFormat("%I %p");
+  //
+  // // test
+  // let test = timestampToHour(incomingData[5].location);
+  // console.log(test);
+  // console.log(typeof(test));
+
+
 
 }
 
