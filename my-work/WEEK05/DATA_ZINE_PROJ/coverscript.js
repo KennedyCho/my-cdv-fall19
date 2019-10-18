c0b133455eeffbf6e2f73f0dd12338dfcec4c8e3
Kennedy Cambra-Cho <kennedy.cambracho@gmail.com>
2019-10-18 18:25:38 +0800

    Update coverscript.js

--- my-work/WEEK05/DATA_ZINE_PROJ/coverscript.js
+++ my-work/WEEK05/DATA_ZINE_PROJ/coverscript.js
@@ -29,18 +29,6 @@ function getColor(datapoint) {
 
 }
 
-
-// function getShape(d) {
-//   // console.log(d);
-//   if (d.withCompany == 'yes') {
-//     shape = document.createElement("circle")
-//     return shape
-//   }else if (d.withCompany == 'no') {
-//     shape = document.createElement("rect")
-//     return shape
-//   }
-// }
-
 function getSVG(d) {
   if (d.typeOfSurface == 'mirror') {
     return svgMirror;
@@ -61,13 +49,15 @@ function gotData(incomingData){
   let gridGroup = viz.selectAll('.gridGroup').data(incomingData).enter()
                       .append('g')
                         .classed('gridGroup', true)
-                        .attr("transform", "translate(200, 50)")
+                        // .attr("transform", "translate(200, 50)")
   ;
+
 
   let shapeGroups = gridGroup.append('g')
                                   .classed('shapeGroup', true)
+                                  .attr("transform", "translate(200, 50)")
 
-                                  // .attr("tranlate", "translate(80,0)")
+
   ;
   var counter = 0;
 
@@ -102,71 +92,36 @@ function gotData(incomingData){
               .attr("cx", getX)
               .attr("cy", getY )
               .attr("r", 30)
+              .attr('fill-opacity', 0.6)
 
 
   ;
-
-  // let rectangles = gridGroup.selectAll("rect")
-  //                             .style("fill", getColor)
-  //                             .attr("x", 200)
-  //                             .attr("y", 200)
-  //                             .attr("width", 100)
-  //                             .attr("height", 100)
-  //
-  // ;
 
   let accentGroups = gridGroup.append('g')
                               .classed('accentGroup', true)
+                              .attr("transform", "translate(150, 0)")
 
   ;
 
-
   var a = 0;
-
-  function accentX(i) {
-    if (i%9 == 0) {
-      a = 0;
-      return a;
-    }else {
-      a = a + 1;
-      return a * 100;
-      // console.log(counter);
-    }
-  }
-
   var b = 0;
-  function accentY(i) {
-
-    if (i==0) {
-      return b;
-    }
-    else if (i%9 == 0) {
-      b = b + 1;
-      // console.log(counter2);
-      return b * 100;
-    }else {
-      return b * 100;
-    }
-  }
-  xscale = d3.scaleLinear().domain([0,9]).range([10, w]);
-  yScale = d3.scaleLinear().domain([0,9]).range([200, h]);
-
-  function getTranslate(d, i) {
-    console.log(i);
-    let x = xscale(i);
-    console.log('this is x',x);
-    let y = yScale(i);
-    console.log('this is y',y);
-    // let position = "translate(" + x + ', ' + y + ")";
-    // return position
-    // "translate(getX, getY)"
-  }
-
   accentGroups.html(getSVG);
   accentGroups.selectAll("path")
               .attr('transform', 'scale(1)')
+              .attr("transform", function (d, i) {
+                if (a < 8) {
+                  console.log(a);
+                  a = a + 1;
+                  return "translate("+ a*100 +"," + b*100+")"
+                } else {
+                  a=0
+                  b = b + 1;
+                  console.log(b);
+                  return "translate("+ a*100 +"," + b*100+")"
+                }
+
+              })
   ;
-  accentGroups.attr('transform', getTranslate);
 
 
 }
@@ -176,18 +131,3 @@ let svgPhone = '<path d="m 25 25 L 75 75 M 75 25 L 25 75" fill="none" stroke="#0
 let svgMirror = '<path d="m 50 10 l 0 30 M 45 10 l 0 30 M 55 10 l 0 30 M 50 60 l 0 30 M 45 60 l 0 30 M 55 60 l 0 30" fill="#ffff00" stroke="#000000" stroke-width="1" />';
 let svgWindow = '<path d="m 75 10 L 25 75 M 80 15 L 30 80 M 70 5 L 20 70 m 0 0 m 0 0 m 0 0 m 0 0" fill="#ffff00" stroke="#000000" stroke-width="1" />';
 let svgLaptop = '<path d="m 10 50 L 30 50 m 40 0 l 20 0 m -10 -10 l -60 0 m 0 20 l 60 0 m 0 0 m 0 0" fill="#ffff00" stroke="#000000" stroke-width="1" />';
-
-
-// function publicGridCellPositions() {
-//
-//                 // We store the top left positions of a 7 by 5 grid. These positions will be our reference points for drawing
-//                 // various objects such as the rectangular grids, the text indicating the date etc.
-//                 var cellPositions = [];
-//                 for (y = 0; y < 5; y++) {
-//                     for (x = 0; x < 7; x++) {
-//                         cellPositions.push([x * publicCellWidth(), y * publicCellHeight()]);
-//                     }
-//                 }
-//
-//                 return cellPositions;
-//             }
