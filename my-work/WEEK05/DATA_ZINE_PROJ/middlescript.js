4610b5414a71d6e58f994295a4356b2b2541781a
Kennedy Cambra-Cho <kennedy.cambracho@gmail.com>
2019-10-18 12:40:54 +0800

    test

--- my-work/WEEK05/DATA_ZINE_PROJ/middlescript.js
+++ my-work/WEEK05/DATA_ZINE_PROJ/middlescript.js
@@ -16,29 +16,14 @@ function gotData(incomingData){
   // y-axis is freq of use
   // freq calc avg use
   // line color denotes medium
-  var parseDate = d3.timeFormat('%H');
+  let xPadding = 10;
+  let yPadding = 10;
 
-  // let date = parseDate(new Date(incomingData[0].timestamp));
-  // console.log(parseInt(date.substring(11,13)));
-  let xPadding = 50;
+  let xScale = d3.scaleLinear().domain([0,100]).range([xPadding, 2400-xPadding]);
+  let yScale = d3.scaleLinear().domain([0,100]).range([xPadding, 2400-xPadding]);
 
-  let alternativeXDomain = d3.extent(incomingData, function(d){
-    console.log(parseDate(d.timestamp));
-    return parseDate(d.timestamp);
-  })
-
-
-  console.log(parseDate);
-  let xScale = d3.scaleTime().domain(alternativeXDomain).range([xPadding, w-(xPadding*2)]);
-  var xAxis = d3.axisBottom(xScale)
-                  .tickFormat(customTimeFormat);
-  ;
-
-  let xAxisGroup = viz.append("g").attr("class", "xaxis");
-  xAxisGroup.call(xAxis);
-
-  let xAxisYPos = h - 30;
-  xAxisGroup.attr("transform", "translate(0,"+xAxisYPos+")");
+  let xAxis = d3.axisBottom(xScale);
+  let yAxis = d3.axisLeft(yScale);
 }
 
 d3.json("data.json").then(gotData);
